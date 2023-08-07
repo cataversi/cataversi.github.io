ff496f942102f49675bcc19ce4d141caf1098753
Cătălin Trașcă <cata.versi@gmail.com>
2023-08-07 15:52:40 +0300

    Update script.js

--- seriale/web-res/script.js
+++ seriale/web-res/script.js
@@ -1,36 +1,47 @@
-    document.addEventListener('DOMContentLoaded', function () {
-      const videoPlayerContainer = document.getElementById('videoPlayerContainer');
-      const videoPlayer = document.getElementById('videoPlayer');
-      const playlistItems = document.querySelectorAll('.playlist li');
+document.addEventListener('DOMContentLoaded', function () {
+  const videoPlayerContainer = document.getElementById('videoPlayerContainer');
+  const videoPlayer = document.getElementById('videoPlayer');
+  const playlistItems = document.querySelectorAll('.playlist li');
 
-      let hls;
+  // Function to play the video using HLS.js
+  function playVideoWithHls(sourceUrl) {
+    if (Hls.isSupported()) {
+      let hls = new Hls();
+      hls.loadSource(sourceUrl);
+      hls.attachMedia(videoPlayer);
+      videoPlayer.play();
+    } else {
+      console.error('HLS is not supported in this browser.');
+    }
+  }
 
-      function playVideo(sourceUrl) {
-        if (hls) {
-          hls.destroy();
-        }
+  // Function to play the video directly in Safari
+  function playVideoInSafari(sourceUrl) {
+    if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
+      videoPlayer.src = sourceUrl;
+      videoPlayer.play();
+    } else {
+      console.error('M3U8 video format is not supported in this browser.');
+    }
+  }
 
-        hls = new Hls();
-        hls.loadSource(sourceUrl);
-        hls.attachMedia(videoPlayer);
-        videoPlayer.play();
-      }
+  // Function to initialize the playlist
+  function initializePlaylist() {
+    playlistItems.forEach(item => {
+      item.addEventListener('click', () => {
+        const sourceUrl = item.getAttribute('data-src');
 
-      // Function to check if HLS.js is loaded before initializing the playlist
-      function initializePlaylist() {
-        if (window.Hls) {
-          playlistItems.forEach(item => {
-            item.addEventListener('click', () => {
-              const sourceUrl = item.getAttribute('data-src');
-              playVideo(sourceUrl);
-            });
-          });
+        // Check if the browser is Safari, then play the video directly
+        if (navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')) {
+          playVideoInSafari(sourceUrl);
         } else {
-          // Retry after 100ms if HLS.js is not loaded yet
-          setTimeout(initializePlaylist, 100);
+          // For other browsers, play the video using HLS.js
+          playVideoWithHls(sourceUrl);
         }
-      }
-
-      // Call the function to initialize the playlist
-      initializePlaylist();
+      });
     });
+  }
+
+  // Call the function to initialize the playlist
+  initializePlaylist();
+});
